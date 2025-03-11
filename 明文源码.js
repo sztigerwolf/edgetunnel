5a0cbe3d93ff8fa94e7936ce250dfc4cf27c25fa
sztigerwolf <sztigerwolf@gmail.com>
2025-03-11 14:12:50 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = '1339d12e-327f-4dc7-b8bf-c0e603de158f';
+let userID = '927d4bf5-f48b-40bc-a4b2-a2782f0e8e35';
 
 // 生成配置文件的 Cloudflare 优选 IP (www.gov.se   speed.cloudflare.com等使用Cloudflare CDN的网站也可以)
 const bestCFIP = "www.visa.com.hk"
